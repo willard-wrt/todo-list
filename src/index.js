27b5b65a73bc63a61a770fefe8d55c73fb4908e6
willard-wrt <119113615+willard-wrt@users.noreply.github.com>
2023-04-10 14:53:20 +0700

    Create functions that create/render project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import './style.css';
 
 // console.log('webpack is working');
+const projectContainer = document.querySelector('#project-titles');
+
 const projectStorage = [];
 const createProject = (name, description) => {
   projectStorage.push({ name, description, toDoList: [], active: true });
+  createToDo('Default Task', 'xx-xx-xx');
+  renderProjects();
 };
 
-const createTodo = (name, dueDate) => {
+const createToDo = (name, dueDate) => {
   activeProject().toDoList.push({ name, dueDate, complete: false });
 };
 
@@ -15,3 +19,45 @@ function activeProject() {
   let activeProjectArray = projectStorage.filter((project) => project.active);
   return activeProjectArray[0];
 }
+
+function switchActiveProject(index) {
+  _clearActiveProjects();
+  projectStorage[index].active = true;
+  // renderProjects();
+  // renderHeading();
+  // renderTodo();
+}
+function _clearProject() {
+  projectContainer.innerHTML = '';
+}
+function _clearActiveProjects() {
+  projectStorage.forEach((project) => (project.active = false));
+}
+
+function renderProjects() {
+  _clearProject();
+  projectStorage.forEach((project, index) => {
+    const projects = document.createElement('div');
+    projects.dataset.value = index;
+    projects.classList.add('project');
+    if (project.active) {
+      projects.classList.add('active');
+    }
+    projects.textContent = project.name;
+    projectContainer.appendChild(projects);
+  });
+}
+
+function createProjectbtnListeners() {
+  const projects = document.querySelectorAll('.project');
+  projects.forEach((btn) =>
+    btn.addEventListener('click', (e) => {
+      switchActiveProject(e.target.dataset.value);
+    })
+  );
+}
+
+/* 
+createProject('Default-Project', 'An Example Project');
+createToDo('test', 'test');
+*/
