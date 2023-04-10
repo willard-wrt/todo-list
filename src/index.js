30e76a3fbcc3a16524f67e9b32e2e9546b2f1e0f
willard-wrt <119113615+willard-wrt@users.noreply.github.com>
2023-04-10 13:58:53 +0700

    Add styles css for project with active class

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './style.css';
 
-console.log('webpack is working');
+// console.log('webpack is working');
+const projectStorage = [];
+const createProject = (name, description) => {
+  projectStorage.push({ name, description, toDoList: [], active: true });
+};
+
+const createTodo = (name, dueDate) => {
+  activeProject().toDoList.push({ name, dueDate, complete: false });
+};
+
+function activeProject() {
+  // eslint-disable-next-line prefer-const
+  let activeProjectArray = projectStorage.filter((project) => project.active);
+  return activeProjectArray[0];
+}
